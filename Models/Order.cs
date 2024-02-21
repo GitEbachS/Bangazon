6ba7da139ec9d0da17bc80752b34b39277e499ef
Shari Ebach <ebachshari@yahoo.com>
2024-02-21 16:40:48 -0600

    setup

---
+++ Models/Order.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bangazon.Models;
+
+public class Order
+{
+    public int Id { get; set; }
+    [Required]
+    public int CustomerId { get; set; }
+    public string PaymentType { get; set; }
+    public DateTime DateCreated { get; set; }
+    public string shipping { get; set; }
+    public bool IsClosed { get; set; }
+}
